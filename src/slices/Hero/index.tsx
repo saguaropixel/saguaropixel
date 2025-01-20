f5342e1337623fcefc8d80b7b81dbecb0cd34367
Sam Harvey <saguaropixel@gmail.com>
2025-01-20 09:39:12 -0700

    Creation of first slice and page

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,26 @@
+import { Content } from "@prismicio/client";
+import { PrismicNextLink } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <PrismicRichText field={slice.primary.heading} />
+      <PrismicRichText field={slice.primary.body} />
+      <PrismicNextLink field={slice.primary.button} />
+    </section>
+  );
+};
+
+export default Hero;
